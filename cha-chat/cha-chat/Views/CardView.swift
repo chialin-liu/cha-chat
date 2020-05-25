1b2a22519432792cd8dc221c41dfb8547851168c
liuchialin <charles.ee96@g2.nctu.edu.tw>
2020-05-25 09:06:29 +0800

    add pan gesture dismiss

--- cha-chat/cha-chat/Views/CardView.swift
+++ cha-chat/cha-chat/Views/CardView.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class CardView: UIView {
     let imageView = UIImageView(image: UIImage(named: "thor"))
+    let threshold: CGFloat = 100
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.clipsToBounds = true
@@ -26,28 +27,47 @@ class CardView: UIView {
         let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
         addGestureRecognizer(panGesture)
     }
+    fileprivate func handleEnded(_ gesture: UIPanGestureRecognizer) {
+        var rightDismiss = false
+        var leftDismiss = false
+        let translation = gesture.translation(in: self)
+        rightDismiss = translation.x > threshold ? true: false
+        leftDismiss = translation.x * -1 > threshold ? true: false
+        UIView.animate(withDuration: 0.8, delay: 0, options: .curveEaseOut, animations: {
+            if rightDismiss {
+                self.transform = CGAffineTransform(translationX: 1000, y: 0)
+            } else if leftDismiss {
+                self.transform = CGAffineTransform(translationX: -1000, y: 0)
+            } else {
+                self.transform = CGAffineTransform.identity
+            }
+        }) { (_) in
+            gesture.setTranslation(.zero, in: self)
+            self.transform = .identity
+        }
+    }
+    
+    fileprivate func handleChanged(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: self)
+        //method 1
+        //            self.center = CGPoint(x: self.center.x + translation.x, y: self.center.y + translation.y)
+        //            gesture.setTranslation(.zero, in: self)
+        //method 2
+        //            self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+        //method 3
+        self.transform = CGAffineTransform.identity.translatedBy(x: translation.x, y: translation.y)
+    }
+    
     @objc func handlePan(gesture: UIPanGestureRecognizer) {
         switch gesture.state {
         case .changed:
-            let translation = gesture.translation(in: self)
-            //method 1
-//            self.center = CGPoint(x: self.center.x + translation.x, y: self.center.y + translation.y)
-//            gesture.setTranslation(.zero, in: self)
-            //method 2
-//            self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
-            //method 3
-            self.transform = CGAffineTransform.identity.translatedBy(x: translation.x, y: translation.y)
+            handleChanged(gesture)
             
         case .ended:
-            UIView.animate(withDuration: 0.8, delay: 0, options: .curveEaseOut, animations: {
-                self.transform = CGAffineTransform.identity
-                
-            }) { (_) in
-                gesture.setTranslation(.zero, in: self)
-            }
+            handleEnded(gesture)
             
         default:
-            ()
+            break
         }
         
         
