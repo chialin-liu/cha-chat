bbe9d4bee66a60fc7f6528367663ce9f0241c2a7
liuchialin <charles.ee96@g2.nctu.edu.tw>
2020-06-03 12:23:07 +0800

    refactor the register for MVVM

--- cha-chat/cha-chat/ViewModels/RegisterViewModel.swift
+++ cha-chat/cha-chat/ViewModels/RegisterViewModel.swift
@@ -51,27 +51,44 @@ class RegisterViewModel {
                 completion(err)
                 
             }
-            let filename = UUID().uuidString
-            let ref = Storage.storage().reference(withPath: "/images/\(filename)")
-            let imageData = self.bindableImage.value?.jpegData(compressionQuality: 0.9) ?? Data()
-            ref.putData(imageData, metadata: nil) { (_, err) in
-                if let err = err {
-//                    self.showLoadingHUD(error: err)
-                    completion(err)
-                }
-                ref.downloadURL { (url, err) in
-                    if let err = err {
-                        completion(err)
-                    }
-                    self.bindableIsRegistering.value = false
-                    print("Download url", url)
-                }
-            }
+            self.saveImageToFirebase(completion: completion)
+            
             
             if let user = res?.user {
                 let uid = user.uid
                 let email = user.email
                 print("User info", uid, "email:", email ?? "")
+            }
+        }
+    }
+    func saveInfoToFirestore(imageUrl: String, completion: @escaping (Error?) -> ()) {
+        let uid = Auth.auth().currentUser?.uid
+        let docData = ["fullName": fullName ?? "", "uid": uid, "imageUrl": imageUrl]
+        Firestore.firestore().collection("users").addDocument(data: docData) { (err) in
+            if let err = err {
+                completion(err)
+                return
+            }
+            completion(nil)
+        }
+    }
+    func saveImageToFirebase(completion: @escaping (Error?) -> ()) {
+        let filename = UUID().uuidString
+        let ref = Storage.storage().reference(withPath: "/images/\(filename)")
+        let imageData = self.bindableImage.value?.jpegData(compressionQuality: 0.9) ?? Data()
+        ref.putData(imageData, metadata: nil) { (_, err) in
+            if let err = err {
+//                    self.showLoadingHUD(error: err)
+                completion(err)
+            }
+            ref.downloadURL { (url, err) in
+                if let err = err {
+                    completion(err)
+                }
+                self.bindableIsRegistering.value = false
+                let imageUrl = url?.absoluteString ?? ""
+                self.saveInfoToFirestore(imageUrl: imageUrl, completion: completion)
+                completion(nil)
             }
         }
     }
